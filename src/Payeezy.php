99594e454395c99094b2f05673e1fdf41c155b76
David Maksimov <dmaksi@gmail.com>
2015-09-09 19:09:15 -0500

    Namespaced

--- src/Payeezy.php
+++ src/Payeezy.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Dmaksimov;
+
 class Payeezy
 {
   /**
--- tests/PayeezyTest.php
+++ tests/PayeezyTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('.\src\Payeezy.php');
+require_once('./src/Payeezy.php');
 
 class PayeezyTest extends PHPUnit_Framework_TestCase
 {
